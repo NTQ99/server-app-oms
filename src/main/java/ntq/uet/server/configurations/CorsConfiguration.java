efe8cf9ff8814d44acec735f45b250c5b4adafbd
NTQ99 <quocnt.uet@gmail.com>
2021-06-02 17:41:35 +0700

    fix cors

--- src/main/java/ntq/uet/server/configurations/CorsConfiguration.java
+++ src/main/java/ntq/uet/server/configurations/CorsConfiguration.java
@@ -14,7 +14,7 @@ public class CorsConfiguration
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+                registry.addMapping("/**").allowedOrigins("http://localhost:3000", "https://oms-2021.web.app", "https://oms-app-2021.herokuapp.com");
             }
         };
     }
